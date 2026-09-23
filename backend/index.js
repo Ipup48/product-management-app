55f520619f8e35ac9c9e95217d1d9b7a7b60faeb
Ipup48 <674259022@webmail.npru.ac.th>
2026-09-23 16:35:35 +0700

    CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,22 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import { connectDB } from "./config/database.js";
 dotenv.config();
+import productRouter from "./router/productRouter.js";
 const PORT = process.env.BACKEND_PORT;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+connectDB();
 app.get("/",(req,res)=>{
     return res
       .status(200)
       .send("<h>welcome to Restful API for Product Mangement App</h>");
 });
+app.use("/api/products",productRouter);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on: http://localhost:${PORT}`);
